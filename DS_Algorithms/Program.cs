bd8ec6504787f98009fce1a2f9ffd4c656f54d9c
sharmilaperiyasamy <sharmilaperiyasamy1992@gmail.com>
2022-07-11 08:52:57 +0530

    [Sharmila] permutations

---
+++ DS_Algorithms/Program.cs
@@ -0,0 +1,7 @@
+﻿DS_Algorithms.Permutations permutations = new DS_Algorithms.Permutations();
+
+Console.WriteLine("Enter the string : ");
+string word = Console.ReadLine();
+int len = word.Length;
+Console.WriteLine("Permutations are : ");
+permutations.permute(word, 0, len - 1);
