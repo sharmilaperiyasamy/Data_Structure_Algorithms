e32b56ee7ca40e54833d6c8f45fef234314d621a
sharmilaperiyasamy <sharmilaperiyasamy1992@gmail.com>
2022-07-11 09:31:55 +0530

    [Sharmila] Binary Search the word in file

---
+++ DS_Algorithms/BinarySearch_word.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DS_Algorithms
+{
+    internal class BinarySearch_word
+    {
+        public void search(string str)
+        {
+            string text = File.ReadAllText(@"C:\Users\Lenovo\source\repos\Bridgelabz\Data_Structure_Algorithms\Binary.txt");
+            string[] words = text.Split(' ');
+            Array.Sort(words);
+            int flag = 0;
+            int i = 0, len = words.Length;
+            while (i < len - 1)
+            {
+
+                int cen = (i + len) / 2;
+                if (words[cen].Equals(str) || words[cen + 1].Equals(str) || words[cen - 1].Equals(str))
+                {
+                    Console.WriteLine("Expected Word is found in the file.");
+                    return;
+                }
+                else if (words[cen].CompareTo(str) > 0)
+                    len = cen - 1;
+                else if (words[cen].CompareTo(str) < 0)
+                    i = cen + 1;
+            }
+            if (flag == 0)
+            {
+                Console.WriteLine("Expected word not found in file.");
+            }
+        }
+    }
+}
--- DS_Algorithms/Program.cs
+++ DS_Algorithms/Program.cs
@@ -1,7 +1,26 @@
-﻿DS_Algorithms.Permutations permutations = new DS_Algorithms.Permutations();
+﻿Console.WriteLine("DataStructure Algorithms : \n1.Permutations\n2.Binary Search the word in file\nEnter your option");
+int option = Convert.ToInt32(Console.ReadLine());
 
-Console.WriteLine("Enter the string : ");
-string word = Console.ReadLine();
-int len = word.Length;
-Console.WriteLine("Permutations are : ");
-permutations.permute(word, 0, len - 1);
+switch(option)
+{
+    case 1:
+
+        DS_Algorithms.Permutations permutations = new DS_Algorithms.Permutations();
+        Console.WriteLine("Enter the string : ");
+        string word = Console.ReadLine();
+        int len = word.Length;
+        Console.WriteLine("Permutations are : ");
+        permutations.permute(word, 0, len - 1);
+        break;
+        
+        case 2:
+        DS_Algorithms.BinarySearch_word search = new DS_Algorithms.BinarySearch_word();
+        Console.WriteLine("Enter the word to search : ");
+        string words = Console.ReadLine();
+        search.search(words);
+        break;
+        default:
+        Console.WriteLine("Invalid option");
+        break;
+
+}
