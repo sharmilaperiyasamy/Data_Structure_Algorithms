e458b7a59a65bbf3b17dc72130b7c5d62449ba24
sharmilaperiyasamy <sharmilaperiyasamy1992@gmail.com>
2022-07-11 18:10:14 +0530

    [Sharmila] Anagram Detection

--- DS_Algorithms/Logical_Programs.cs
+++ DS_Algorithms/Logical_Programs.cs
@@ -98,5 +98,25 @@ namespace DS_Algorithms
             for (int i = 0; i < array.Length; i++)
                 Console.WriteLine(array[i]);
         }
+
+        //uc6 anagram detection
+        public void Anagramdetect(string str1, string str2)
+        {
+            char[] ch1 = str1.ToLower().ToCharArray();
+            char[] ch2 = str2.ToLower().ToCharArray();
+            Array.Sort(ch1);
+            Array.Sort(ch2);
+            string val1 = new string(ch1);
+            string val2 = new string(ch2);
+
+            if (val1 == val2)
+            {
+                Console.WriteLine("Both the strings are Anagrams");
+            }
+            else
+            {
+                Console.WriteLine("Both the strings are not Anagrams");
+            }
+        }
     }
 }
--- DS_Algorithms/Program.cs
+++ DS_Algorithms/Program.cs
@@ -1,4 +1,4 @@
-﻿Console.WriteLine("DataStructure Algorithms : \n1.Permutations\n2.Binary Search the word in file\n3.Insertion Sort\n4.Bubble Sort\nEnter your option");
+﻿Console.WriteLine("DataStructure Algorithms : \n0.Exit\n1.Permutations\n2.Binary Search the word in file\n3.Insertion Sort\n4.Bubble Sort\n5.Anagram Detection\nEnter your option");
 int option = Convert.ToInt32(Console.ReadLine());
 
 DS_Algorithms.Logical_Programs program = new DS_Algorithms.Logical_Programs();
@@ -6,23 +6,26 @@ DS_Algorithms.Logical_Programs program = new DS_Algorithms.Logical_Programs();
 switch (option)
 {
     case 1:
+        Console.WriteLine("Permutations");
         Console.WriteLine("Enter the string : ");
         string word = Console.ReadLine();
         int len = word.Length;
         Console.WriteLine("Permutations are : ");
         program.permute(word, 0, len - 1);
         break;
-        
-        case 2:
+
+    case 2:
+        Console.WriteLine("Binary Search the word in file");
         Console.WriteLine("Enter the word to search : ");
         string words = Console.ReadLine();
         program.search(words);
         break;
     case 3:
+        Console.WriteLine("Insertion Sort");
         Console.WriteLine("Enter number of words to insert : ");
         int count = Convert.ToInt32(Console.ReadLine());
         string[] array = new string[count];
-        for (int i =0; i < count; i++)
+        for (int i = 0; i < count; i++)
         {
             Console.WriteLine("Enter the string to add : ");
             string s = Console.ReadLine();
@@ -31,10 +34,11 @@ switch (option)
         program.InsertionSort(array);
         break;
     case 4:
+        Console.WriteLine("Bubble Sort");
         Console.WriteLine("Enter number of values to add : ");
         int n = Convert.ToInt32(Console.ReadLine());
         int[] arr = new int[n];
-        for(int i = 0; i < n; i++)
+        for (int i = 0; i < n; i++)
         {
             Console.WriteLine("Enter the numbers to add : ");
             int x = Convert.ToInt32(Console.ReadLine());
@@ -42,8 +46,16 @@ switch (option)
         }
         program.BubbleSort(arr);
         break;
-        default:
+    case 5:
+        Console.WriteLine("Anagram Detection");
+        Console.WriteLine("Enter the string1:");
+        string a1 = Console.ReadLine();
+        Console.WriteLine("Enter the string2:");
+        string a2 = Console.ReadLine();
+        program.Anagramdetect(a1, a2);
+        break;
+    default:
         Console.WriteLine("Invalid option");
         break;
-
 }
+
